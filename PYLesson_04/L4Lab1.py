99b426d508d72b61016d83007551aaa5bdda43b3
JLevy22 <julianlevy0@gmail.com>
2016-09-19 10:26:46 -0700

    some new stuff

---
+++ PYLesson_04/L4Lab1.py
@@ -0,0 +1,11 @@
+print ("Lab Part 1:Receipt")
+
+
+item1=input("Please enter the first item:")
+item1price=int(input("Please enter the price:"))
+item2=input("Please enter the second item:")
+item2price=int(input("Please enter the price:"))
+item3=input("Please enter the third item:")
+item3price=int(input("Please enter the price:"))
+
+print ("_________<Receipt>")
---
+++ PYLesson_04/Lesson4cont.py
@@ -0,0 +1,15 @@
+
+
+def printf(word , number):
+    print ("{:<6}\t{:10.2f}".format(word , number))
+
+
+word="blah"
+number= 34564.11
+
+printf(word , number)
+
+word="snapers"
+number= 42324.665
+
+printf(word , number)
