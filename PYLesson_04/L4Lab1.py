f92c5f6231c0114bbb41a272b17e2c4101b16dd6
JLevy22 <julianlevy0@gmail.com>
2016-09-21 10:26:51 -0700

    Almost finished

--- PYLesson_04/L4Lab1.py
+++ PYLesson_04/L4Lab1.py
@@ -1,11 +1,25 @@
 print ("Lab Part 1:Receipt")
-
-
+    
+def format (item,price):
+    print ("{:>15}\t{:10.2f}".format(item,price))
+    
 item1=input("Please enter the first item:")
-item1price=int(input("Please enter the price:"))
+item1price=float(input("Please enter the price:"))
 item2=input("Please enter the second item:")
-item2price=int(input("Please enter the price:"))
+item2price=float(input("Please enter the price:"))
 item3=input("Please enter the third item:")
-item3price=int(input("Please enter the price:"))
+item3price=float(input("Please enter the price:"))
 
-print ("_________<Receipt>")
+subtotal= item1price+item2price+item3price
+tax= subtotal*.08+subtotal
+total= tax+subtotal
+
+print ("__________<Receipt>___________")
+format (item1,item1price)
+format (item2,item2price)
+format (item3,item3price)
+print (" ")
+format ("Subtotal:", subtotal)
+format ("Tax:", tax)
+format ("Total:", total)
+print ("______________________________")
