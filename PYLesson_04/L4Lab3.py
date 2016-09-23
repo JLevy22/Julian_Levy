ac1285952a0b84c4865f54e2fba35ce5912978c1
JLevy22 <julianlevy0@gmail.com>
2016-09-23 10:25:28 -0700

    finished lab 1,2,3

---
+++ PYLesson_04/L4Lab3.py
@@ -0,0 +1,21 @@
+print ("Lab Part 3: Compound Intrest")
+print (" ")
+
+def format (stat,A,dollars):
+    print ("{:20}{:<10.2f}{:<2}".format (stat,A,dollars))
+
+dollars= ("$")
+r=float(input("What is the intrest rate:"))
+p=float(input("What is the principal amount:"))
+n=float(input("How many times is it compounded per year:"))
+t=float(input("How many years:"))
+print (" ")
+
+A1= 1+r/n
+A2= n*t
+A3= A1**A2
+A4= A3*p
+
+
+format("The total amount is:",A4,dollars)
+
