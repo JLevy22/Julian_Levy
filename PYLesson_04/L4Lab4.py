3ecd73f59b69b221a378685bf4fa1b290593ca33
JLevy22 <julianlevy0@gmail.com>
2016-09-27 10:25:03 -0700

    Finished some stuff

---
+++ PYLesson_04/L4Lab4.py
@@ -0,0 +1,17 @@
+print ("Lab Part 4: Subwoofer Boxes:")
+print (" ")
+
+def format (volume):
+    print ("{:10.4f}".format (volume))
+
+h= float(input("Please enter the hight in inches:"))
+l= float(input("Please enter the length in inches:"))
+w= float(input("please enter the width in inches:"))
+
+inchvol = h*l*w
+format ("The volume in cubic inches is:",inchvol)
+
+cubef = 12*12*12
+
+feetvol = inchvol/cubef
+format ("The volume in cubic feet is:",feetvol)
